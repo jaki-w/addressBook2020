e2b8cc5dbcfd7d2c8d0ac8beba17d12dfdf95338
Jaki Widby <jaki.widby@gmail.com>
2020-08-23 15:16:41 -0700

    create deleteContact prototype to delete contact by id

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,20 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
-AddressBook.prototype.findConact = function(id) {
+AddressBook.prototype.findContact = function(id) {
   for (let i=0; i<this.contacts.length; i++) {
     if (this.contacts[i].id == id) {
       return this.contacts[i];
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i<this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      delete this.contacts[i];
+      return true;
     }
   };
   return false;
